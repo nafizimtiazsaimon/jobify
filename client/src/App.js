35c5d9208f17e20aded4538247695851b5da8a48
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-01 00:09:24 +0600

    Logo Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
 import Landing from "./pages/Landing";
 import styled from 'styled-components';
 
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 1rem;
-`
-
 function App() {
   return (
     <div>
-      <Button>Click ME</Button>
       <h1>jobify</h1>
       <Landing/>
     </div>
