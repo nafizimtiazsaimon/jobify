13a5a36b4fd5711cdc66e75a37d62e7482bb9199
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-01 15:20:01 +0600

    Link Component

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main-alternative.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import { Logo } from '../components'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return(
@@ -16,7 +17,7 @@ const Landing = () => {
             <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
-            <button className='btn btn-hero'>Login/Register</button>
+            <Link className='btn btn-hero'>Login/Register</Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
     </div>
