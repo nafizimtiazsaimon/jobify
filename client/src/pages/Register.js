348cddeb990dcfa05f0b4d7be939f05093b1832e
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-02 23:59:05 +0600

    Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { Logo } from '../components'
+import { Logo,FormRow } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
 
 const initialState = {
@@ -25,12 +25,24 @@ const Register = () => {
     <form className="form" onSubmit={onSubmit}>
       <Logo />
       <h3>Login</h3>
-      <div className='form-row'>
-        <label htmlFor="name" className="form-label">
-          name
-        </label>
-        <input type="text" value={values.name} name="name" onChange={handleChange} className="form-input"/>
-      </div>
+      <FormRow 
+        type="text" 
+        name="name" 
+        value={values.name} 
+        handleChange={handleChange}
+      />
+      <FormRow 
+        type="email" 
+        name="email" 
+        value={values.email} 
+        handleChange={handleChange}
+      />
+      <FormRow 
+        type="password" 
+        name="password" 
+        value={values.password} 
+        handleChange={handleChange}
+      />
       <button type="submit" className="btn btn-block">Submit</button>
     </form>
     </Wrapper>
