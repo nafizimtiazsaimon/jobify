1b8f36bef17f9d6b7e173207d3d41eda50984c15
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-02 23:26:51 +0600

    Register page Name
    Designing the register page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -17,7 +17,7 @@ const Landing = () => {
             <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
-            <Link to='/register' className='btn btn-hero'>Login/Register</Link>
+            <Link to='/register ' className='btn btn-hero'>Login/Register</Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
     </div>
