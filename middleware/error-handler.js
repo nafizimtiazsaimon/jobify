39d16347591ed2e341ea3af0b2516f6b9521e669
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-07 18:30:45 +0600

    Connected to Database

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  //console.log(err);
   res.status(500).json({ msg: "error" });
 };
 
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
 app.get("/", (req, res) => {
-  throw new Error("error");
+  //throw new Error("error");
   res.send("Hello");
 });
 
@@ -13,4 +17,16 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Connected to ${port}`)
+});
+
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URL)
+  }catch (error){
+    console.log(error)
+  }
+}
+
+start()
