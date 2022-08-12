4775b8b43a466eb095c4a399dabfca98e571c7c8
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-12 23:17:41 +0600

    Auth Structure

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import dotenv from "dotenv";
+import authRouter from './routes/authRoutes.js'
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
   //throw new Error("error");
   res.send("Hello");
 });
+
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
